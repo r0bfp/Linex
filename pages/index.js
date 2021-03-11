c55473e0edb283d305896b505da4b398a4eedcff
r0bfp <robfpinheiro@gmail.com>
2021-03-10 22:45:23 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <main>
+        <h1>teste next</h1>
+      </main>
+    </div>
+  )
+}
